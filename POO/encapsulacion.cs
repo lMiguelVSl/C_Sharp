0771c65c3ecf88dbafae92e54ed5b9390f62d863
Miguel Angel Vargas <sanchez.vc@hotmail.com>
2022-04-18 13:21:10 -0500

    Encapsulacion

---
+++ POO/encapsulacion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cursoC_Sharp_POO
+{
+    class encapsulacion
+    {
+        static void Main(string[] args)
+        {
+            SampleClass p1 = new SampleClass("Miguel"); //cuando se tiene el contructor se envia como parametro el valor deseado
+            // p1.  no deja ver el campo ya que es de tipo private
+            
+            Console.WriteLine(p1.SampleProperty);
+
+
+            SampleClass person2 = new SampleClass("Juan pablo", "Valencia"); //se tiene el contructor asi que se le envian los dos parametros que pide 
+            Console.WriteLine(person2.GetFullName());
+        }
+    }
+    public class SampleClass
+    {
+        public SampleClass (string _sampleClass) //contructor para objetos con solo un nombre
+        {
+            _sampleClass = _sampleClass;
+        }
+        public SampleClass(string FirsName, string _secondName) //constructor para objetos con dos nombres
+        {
+            FirstName = FirsName;
+            SecondName = _secondName;
+        }
+        private string _sampleClass;
+        private string _secondName;
+        public string FirstName { get; set; }
+        public string SampleProperty //c# permite crear campos privado para almacenar el valor 
+        {
+            get { return _sampleClass; }
+            set { _sampleClass = value; }
+        }
+        public string SecondName 
+        {
+            get { return _secondName; }
+            set { _secondName = value; }
+
+        }
+        public string GetFullName() //metodo 
+        {
+            return $"{FirstName} {SecondName}";
+        }
+
+        //una clase puede tener una sobre carga de metodos
+    }
+}
--- POO/firstClassPOO.cs
+++ POO/firstClassPOO.cs
@@ -1,22 +1,39 @@
 ﻿using System;
 
 namespace cursoC_Sharp
-{
+{ /*
     class firstClass_POO
-    {
+    { 
         static void Main(string[] args)
         {
-            PersonClass p = new PersonClass();
+            PersonClass p = new PersonClass(); //apunta a un espacio de memoria
             PersonClass p1 = new PersonClass();
 
             p.Name = "Miguel";
-            Console.WriteLine(p1.Name);
+            Console.WriteLine($"p name is {p.Name}");
 
-            p1 = p;
-            Console.WriteLine(p1.Name);
+            p1 = p; //p1 apunta al mismo espacio de memoria ya que se apunto al mismo valor de p
+            Console.WriteLine($"if p1=p the p1 name is {p1.Name}");
 
-            p1.Name = "Alejandro";
+            p1.Name = "Alejandro"; //en class al modificar el valor de una variable que apunta a otra, se cambia el valor de la propiedad para ambas
             Console.WriteLine($"P name is {p.Name} and p1 name is {p1.Name}");
+            Console.ReadKey();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            PersonStruct personStruct = new PersonStruct();
+            PersonStruct personStruc2 = new PersonStruct();
+
+            Console.WriteLine("Now creating a struct object");
+            personStruct.Name = "Juan pablo";
+            Console.WriteLine($"the name is {personStruct.Name}");
+
+            personStruc2 = personStruct; //le asigno las mismas propiedades a la 2 
+            Console.WriteLine($"The personStruct2's name is {personStruc2.Name}");
+
+            personStruc2.Name = "Miguelinho";
+            Console.WriteLine($"The personStruct2's name is {personStruc2.Name} and the first one is {personStruct.Name}");
+            Console.ReadKey();
+
         }
     }
     public class PersonClass
@@ -24,4 +41,8 @@ namespace cursoC_Sharp
         public string Name { get; set; }
         public int MyProperty { get; set; }
     }
+    public struct PersonStruct
+    {
+        public string Name { get; set; }
+    } */
 }
