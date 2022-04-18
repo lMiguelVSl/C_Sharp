f86574d9a462eb2eb9b8f94b56aa327cc9ff72db
Miguel Angel Vargas <sanchez.vc@hotmail.com>
2022-04-18 10:27:27 -0500

    Class exceptions

---
+++ cursoC_Sharp/exceptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cursoC_Sharp
+{
+    public class exceptions
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Por favor introduce un número para dividir 100: ");
+            Divide(Console.ReadLine());
+            Console.ReadKey();
+        }
+
+        static void Divide(string stringNum)
+        {
+            try
+            {
+                int num = int.Parse(stringNum);
+                int result = 100 / num;
+                Console.WriteLine($"100 / {num} = {result}");
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Se ha generado una excepción del tipo {ex.GetType().Name}");
+            }
+        }
+    }
+}
