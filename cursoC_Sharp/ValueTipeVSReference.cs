e08e4bfc2af449c4090b486d343d0f147a233ec4
Miguel Angel Vargas <sanchez.vc@hotmail.com>
2022-04-24 22:27:12 -0500

    Reference tipes and value (Memory) Practice

---
+++ cursoC_Sharp/ValueTipeVSReference.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cursoC_Sharp
+{
+    public class ValueTipeVSReference
+    {
+        public static void Main(string[] args)
+        {
+            //tipo de valor 
+            int index = 7;
+            Console.WriteLine($"The index value before the plus is: {index}"); 
+            Console.WriteLine($"The total plus is: {Sum(index)}");
+            Console.WriteLine($"The index value after the plus is: {index}"); //al ser index un tipo de valor, no se ve afectado el valor de la variable despues de sumarle
+            Console.ReadKey();
+
+            Console.WriteLine("Now the reference tipe");
+            Person person = new("Miguel", 21);
+
+            Console.WriteLine($"Person name: {person.Name}");
+            changeName(person);
+            Console.WriteLine($"Person name after the method change name: {person.Name}");
+            Console.ReadKey();
+
+        }
+
+        public static int Sum(int index)
+        {
+            return index + 4;
+        }
+
+        //tipos de referencia, todas las clases de objetos de .NET (cadenas,strings) se alojan en el monton, menos eficiente el uso de datos por la forma como se almacena
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+        public static void changeName(Person person)
+        {
+            person.Name = person.Name + " Changed";
+        }
+    }
+}
--- cursoC_Sharp/challengeLists.cs
+++ cursoC_Sharp/challengeLists.cs
@@ -8,43 +8,43 @@ namespace cursoC_Sharp
 {
     public class challengeLists
     {
-        public static void Main(string[] args)
-        {
-            string[] studentsName = {"Mateo","Juan","Miguel" };
-            SortedList<string, int> sortedStudentsScores = new SortedList<string, int>();
-            int score;
-            foreach (string studentName in studentsName)
-            {
-                score = 0;
-                string scoreWrote = "";
-                while (score == 0 )
-                {
-                    Console.WriteLine("write the student score");
-                    scoreWrote = Console.ReadLine();
-                    getInteger(scoreWrote, out score);
-                    while (score == 0)
-                    {
-                        Console.WriteLine("write the student score");
-                        scoreWrote = Console.ReadLine();
-                        getInteger(scoreWrote, out score);
-                    }                   
-                    sortedStudentsScores.Add(studentName, Convert.ToInt32(score));
-                    score++;
+    //    public static void Main(string[] args)
+    //    {
+    //        string[] studentsName = {"Mateo","Juan","Miguel" };
+    //        SortedList<string, int> sortedStudentsScores = new SortedList<string, int>();
+    //        int score;
+    //        foreach (string studentName in studentsName)
+    //        {
+    //            score = 0;
+    //            string scoreWrote = "";
+    //            while (score == 0 )
+    //            {
+    //                Console.WriteLine("write the student score");
+    //                scoreWrote = Console.ReadLine();
+    //                getInteger(scoreWrote, out score);
+    //                while (score == 0)
+    //                {
+    //                    Console.WriteLine("write the student score");
+    //                    scoreWrote = Console.ReadLine();
+    //                    getInteger(scoreWrote, out score);
+    //                }                   
+    //                sortedStudentsScores.Add(studentName, Convert.ToInt32(score));
+    //                score++;
 
-                }
-            }
-            foreach (var item in sortedStudentsScores)
-            {
-                Console.WriteLine(item);
-            }
+    //            }
+    //        }
+    //        foreach (var item in sortedStudentsScores)
+    //        {
+    //            Console.WriteLine(item);
+    //        }
 
-        }
-        public static void getInteger(string data, out int score)
-        {
-            if (!int.TryParse(data, out score))
-            {
-                Console.WriteLine("The data need be a int");
-            }
-        }
+    //    }
+    //    public static void getInteger(string data, out int score)
+    //    {
+    //        if (!int.TryParse(data, out score))
+    //        {
+    //            Console.WriteLine("The data need be a int");
+    //        }
+    //    }
     }
 }
