e4bfa1c08c9f9b604c544ebd72979d55ba75abe4
Miguel Angel Vargas <sanchez.vc@hotmail.com>
2022-04-29 15:39:43 -0500

    ParameterizedThreadStart

--- cursoC_Sharp/DelegateAndLambda.cs
+++ cursoC_Sharp/DelegateAndLambda.cs
@@ -6,50 +6,50 @@ using System.Threading.Tasks;
 
 namespace cursoC_Sharp
 {
-    public class DelegateAndLambda
-    {
-        public void Main(string[] args) //con las expresiones lambda vienen desde c# version 3, las expresiones lambda son solo una forma de expecificar un delegado
-        { //la sintaxis es mas sencilla a difernecia se pueden asignar directamente
-            List<int> list = new List<int>();
+//    public class DelegateAndLambda
+//    {
+//        public void Main(string[] args) //con las expresiones lambda vienen desde c# version 3, las expresiones lambda son solo una forma de expecificar un delegado
+//        { //la sintaxis es mas sencilla a difernecia se pueden asignar directamente
+//            List<int> list = new List<int>();
 
-            for (int i = 1; i <= 100; i++)
-            {
-                list.Add(i);
-            }
+//            for (int i = 1; i <= 100; i++)
+//            {
+//                list.Add(i);
+//            }
 
-            List<int> result = list.FindAll(i => i % 2 == 0); //expresion lambda, sintaxis comoda para los delegados  
+//            List<int> result = list.FindAll(i => i % 2 == 0); //expresion lambda, sintaxis comoda para los delegados  
 
-            foreach (var item in result)
-            {
-                Console.WriteLine(item);
-            }
-            Console.Read();
+//            foreach (var item in result)
+//            {
+//                Console.WriteLine(item);
+//            }
+//            Console.Read();
 
 
-            //public static void Main(string[] args) delegado
-            //{
-            //    List<int> list = new List<int>();
+//            //public static void Main(string[] args) delegado
+//            //{
+//            //    List<int> list = new List<int>();
 
-            //    for (int i = 1; i <= 100; i++)
-            //    {
-            //        list.Add(i);
-            //    }
+//            //    for (int i = 1; i <= 100; i++)
+//            //    {
+//            //        list.Add(i);
+//            //    }
 
-            //    List<int> result = list.FindAll(
-            //      delegate (int no) cuerpo del delegado 
-            //      {
-            //          return (no % 2 == 0);
-            //      }
-            //    );
+//            //    List<int> result = list.FindAll(
+//            //      delegate (int no) cuerpo del delegado 
+//            //      {
+//            //          return (no % 2 == 0);
+//            //      }
+//            //    );
 
-            //    foreach (var item in result)
-            //    {
-            //        Console.WriteLine(item);
-            //    }
+//            //    foreach (var item in result)
+//            //    {
+//            //        Console.WriteLine(item);
+//            //    }
 
-            //    Console.Read();
-            //}
-        }
+//            //    Console.Read();
+//            //}
+//        }
 
-    }
+//    }
 }
