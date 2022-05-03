48ff6ebf42f9661286631fd4595e355869914816
Miguel Angel Vargas <sanchez.vc@hotmail.com>
2022-05-03 11:13:31 -0500

    IEnumerable start

---
+++ POOIntermediateAdvance/IEnumerableImplementation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOIntermediateAdvance
+{
+    public class IEnumerableImplementation
+    {
+        public static void IEnumerableMain()
+        {
+            List<int> numbers = new List<int> { 1,2,3,4,5,6,7,8,9,10}; //creo la lista de valores enteros y le agrego valores
+            using (List<int>.Enumerator enumerator = numbers.GetEnumerator()) //usando una lista de tipo enteros con el metodo Enumerator perteneciente a Ienumerable
+            {                                                                 //le voy a asignar el metodo GetEnumerator aplicado a la lista ya creada
+                while (enumerator.MoveNext()) //mientras exista siguiente valor en la lista 
+                {
+                    Console.WriteLine(enumerator.Current); //vamos a escribir el valor en el que nos encontramos 
+                }
+            }
+        }
+    }
+}
