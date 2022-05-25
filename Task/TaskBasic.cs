e1814c95fe2ecfeb51b86759b22172a4947f74a4
Miguel <sanchez.vc@hotmail.com>
2022-05-25 12:35:58 -0500

    Task Basic

---
+++ Task/TaskBasic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class TaskBasic
+    {
+        public static void TaskBasicMain()
+        {
+            Task t = Task.Run(() => //creacion de la tarea con lambda expresion
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    Console.WriteLine('x');
+                }
+            });
+            t.Wait();
+        }
+    }
+}
