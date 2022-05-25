5cc56a876db87c946f6e4b67e6c95629ece6c4aa
Miguel <sanchez.vc@hotmail.com>
2022-05-25 12:38:32 -0500

    Task with a return value

---
+++ Task/TaskReturn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class TaskReturn
+    {
+        public static void TaskReturnMain()
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 22;
+            });
+            Console.WriteLine(t.Result);
+            Console.ReadKey();
+        }
+      
+    }
+}
